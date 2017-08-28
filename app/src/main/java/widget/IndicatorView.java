5185ff8f9fc3b0edc67d7ff13b379b1f465c00a7
KDisEDDY <LJY68835441@163.com>
2017-08-28 20:01:13 +0800

    1.另一种思路的indicator

--- app/src/main/java/widget/IndicatorView.java
+++ app/src/main/java/widget/IndicatorView.java
@@ -12,7 +12,6 @@ import android.support.v4.view.ViewPager;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.view.View;
-import android.widget.LinearLayout;
 
 /**
  * Title: IndicatorView
@@ -25,40 +24,22 @@ import android.widget.LinearLayout;
  * todo:onPageScrollStateChanged的值变化要关注
  */
 
-public class IndicatorView extends BasePaintView implements ViewPager.OnPageChangeListener,  View.OnAttachStateChangeListener  {
+public class IndicatorView extends BasePaintView implements ViewPager.OnPageChangeListener {
 
     private int mWidth = 0;
     private int mHeight = 0;
 
     private int mPointWidth = 0;
-    private int mPointRadius = 0;
+    private int mPointDiameter = 0;
 
     private ViewPager mPager = null;
     private int mPageCount = 0;
-    private int currentPage = -1;
-    private int previousPage = -1;
-    private boolean pageChanging;
-    private float previousOffset = 0;
-    private int previousState = 0;  //0：不移动；1：左滑；2：右滑；3：左滑恢复原状；4：右滑恢复原状
+    private int mLeftPosition = 0;
 
     private float[] fractions = null;
     private Path[] mPointPaths = null;
     private RectF[] mRectFs = null;
     private Path mCompletePath = null;
-    private boolean isAttachedToWindow;
-
-    Handler handler = new Handler(){
-        @Override
-        public void handleMessage(Message msg) {
-            switch (msg.what){
-                case 1:
-                    IndicatorView.this.invalidate();
-                    break;
-                default:
-                    break;
-            }
-        }
-    };
 
     public IndicatorView(Context context) {
         super(context);
@@ -82,62 +63,14 @@ public class IndicatorView extends BasePaintView implements ViewPager.OnPageChan
         for(int i = 0 ; i < fractions.length ; i++){
             mPointPaths[i].rewind();
             float width = mPointWidth * fractions[i];
-
             if(width <= mPointWidth / 4){
-                width = mPointRadius;
+                width = mPointDiameter;
             }
-            if(previousState == 1){
-                //左滑,当前点右边压缩
-                if(fractions[i] > 0){
-                    if(!pageChanging && i == currentPage){
-                        mRectFs[i] = new RectF(mRectFs[i].left , mRectFs[i].top,mRectFs[i].left + width,mRectFs[i].bottom);
-                    } else if(pageChanging && i == previousPage){
-                        mRectFs[i] = new RectF(mRectFs[i].left , mRectFs[i].top,mRectFs[i].left + width,mRectFs[i].bottom);
-                    } else {
-                        mRectFs[i] = new RectF(mRectFs[i].right - width , mRectFs[i].top,mRectFs[i].right,mRectFs[i].bottom);
-                    }
-                } else {
-                    mRectFs[i] = new RectF(mRectFs[i]);
-                }
-            } else if(previousState == 2){
-                //右滑，当前点左边压缩
-                if(fractions[i] > 0){
-                    if(!pageChanging && i == currentPage){
-                        mRectFs[i] = new RectF(mRectFs[i].right - width , mRectFs[i].top,mRectFs[i].right , mRectFs[i].bottom);
-                    } else if(pageChanging && i == previousPage){
-                        mRectFs[i] = new RectF(mRectFs[i].right - width , mRectFs[i].top,mRectFs[i].right , mRectFs[i].bottom);
-                    } else {
-                        mRectFs[i] = new RectF(mRectFs[i].left , mRectFs[i].top,mRectFs[i].left + width,mRectFs[i].bottom);
-                    }
-                } else {
-                    mRectFs[i] = new RectF(mRectFs[i]);
-                }
-            } else if(previousState == 3){
-                //左滑恢复
-                if(fractions[i] > 0){
-                    if(!pageChanging && i == currentPage){
-                        mRectFs[i] = new RectF(mRectFs[i].left , mRectFs[i].top , mRectFs[i].left + width,mRectFs[i].bottom);
-                    } else if(pageChanging && i == previousPage){
-                        mRectFs[i] = new RectF(mRectFs[i].left , mRectFs[i].top , mRectFs[i].left + width,mRectFs[i].bottom);
-                    } else {
-                        mRectFs[i] = new RectF(mRectFs[i].right - width , mRectFs[i].top,mRectFs[i].right,mRectFs[i].bottom);
-                    }
-                } else {
-                    mRectFs[i] = new RectF(mRectFs[i]);
-                }
-            } else if(previousState == 4){
-                //右滑恢复
-                if(fractions[i] > 0){
-                    if(!pageChanging && i == currentPage){
-                        mRectFs[i] = new RectF(mRectFs[i].right - width , mRectFs[i].top,mRectFs[i].right , mRectFs[i].bottom);
-                    } else if(pageChanging && i == previousPage){
-                        mRectFs[i] = new RectF(mRectFs[i].right - width , mRectFs[i].top,mRectFs[i].right , mRectFs[i].bottom);
-                    } else {
-                        mRectFs[i] = new RectF(mRectFs[i].left , mRectFs[i].top,mRectFs[i].left + width,mRectFs[i].bottom);
-                    }
-                } else {
-                    mRectFs[i] = new RectF(mRectFs[i]);
-                }
+
+            if(i == mLeftPosition){
+                mRectFs[i] = new RectF(mRectFs[i].left,mRectFs[i].top,mRectFs[i].left + width, mRectFs[i].bottom);
+            } else if(i == mLeftPosition + 1) {
+                mRectFs[i] = new RectF(mRectFs[i].right - width,mRectFs[i].top,mRectFs[i].right, mRectFs[i].bottom);
             }
             mPointPaths[i].addRoundRect(mRectFs[i],mPointWidth / 4  , mHeight/4 , Path.Direction.CCW);
             mCompletePath.addPath(mPointPaths[i]);
@@ -162,15 +95,13 @@ public class IndicatorView extends BasePaintView implements ViewPager.OnPageChan
     }
 
     private void initPointState(){
-        currentPage = 0;
-        previousPage = 0;
         fractions = new float[mPageCount];
         mPointPaths = new Path[mPageCount];
         mRectFs = new RectF[mPageCount];
         mCompletePath = new Path();
         mPointWidth = mWidth / mPageCount;
         int pointRight = mPointWidth;
-        mPointRadius = mPointWidth / 4;
+        mPointDiameter = mPointWidth / 4;
         for(int i = 0 ; i < fractions.length ; i++){
             //置第一个点为当前点
             if(i == 0){
@@ -178,7 +109,7 @@ public class IndicatorView extends BasePaintView implements ViewPager.OnPageChan
                 mRectFs[i] = new RectF(0 , 0 , pointRight ,  mHeight / 4);
             } else {
                 fractions[i] = 0f;
-                mRectFs[i] = new RectF( pointRight - mPointRadius, 0 , pointRight , mHeight / 4);
+                mRectFs[i] = new RectF( pointRight - mPointDiameter, 0 , pointRight , mHeight / 4);
             }
             mPointPaths[i] = new Path();
             pointRight = pointRight + mPointWidth;
@@ -198,146 +129,26 @@ public class IndicatorView extends BasePaintView implements ViewPager.OnPageChan
 
     @Override
     public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
-        Log.i("offset",positionOffset + "");
-        boolean lastoffset = false;
-        int currentPosition = pageChanging ? previousPage : currentPage;
-        Log.i("pageChange" , "previousPage:" + previousPage + ",currentPage:" + currentPage);
-        //规避掉左滑时最后一个positionOffset为0的情况
-        if(Math.abs(positionOffset - previousOffset) > 0.8 && previousOffset > 0.9){
-            positionOffset = 1f;
-            lastoffset = true;
-        }
-        //右滑时第一次滑动的情况
-        if(positionOffset > 0.9 && Math.abs(positionOffset - previousOffset) > 0.9){
-            previousOffset = 1f;
-        }
-        if(positionOffset - previousOffset > 0){
-            Log.i("offsetDis","" + (positionOffset - previousOffset));
-            //左滑
-            //根据position的左边的点判断之前的操作是否为右滑,规避掉currentPosition = 0 时的情况
-            if(currentPosition - 1 != -1 && fractions[currentPosition - 1] > 0){
-                setJoiningFraction(currentPosition,positionOffset);
-                setJoiningFraction(currentPosition - 1 , 1 - positionOffset);
-                Log.i("previousFraction","4:curFraction:" + fractions[currentPosition] + "," + "currentPos: " + currentPosition);
-                Log.i("previousFraction","4:PreFraction:" + fractions[currentPosition - 1] + "," + "previousPos: " + (currentPosition - 1));
-                Log.i("previousState" ,"previousState:" + 4);
-                previousState = 4;
+        if(fractions == null) return ;
+        mLeftPosition = position;
+        for(int i = 0 ; i < fractions.length;i++){
+            if(i == position){
+                fractions[i] = 1 - positionOffset;
+            } else if(i == position + 1){
+                fractions[i] = positionOffset;
             } else {
-                setJoiningFraction(currentPosition ,1 - positionOffset);
-                setJoiningFraction(currentPosition + 1 ,positionOffset);
-                Log.i("previousFraction","1:curFraction:" + fractions[currentPosition] + "," + "currentPos: " + currentPosition);
-                Log.i("previousState" ,"previousState:" + 1);
-                previousState = 1;
+                fractions[i] = 0f;
             }
-
-        } else if(positionOffset - previousOffset < 0){
-            //右滑
-            //根据position的右边的点判断之前的操作是否为左滑,规避掉currentPosition = pageCount 时的情况
-            if(currentPosition + 1 < mPageCount && fractions[currentPosition + 1] > 0){
-                Log.i("previousFraction" , "3:preFraction:" + fractions[currentPosition + 1] + ", previousPos" + (currentPosition + 1));
-                setJoiningFraction(currentPosition ,1 - positionOffset);
-                setJoiningFraction(currentPosition + 1 , positionOffset);
-                Log.i("previousFraction","3:curFraction:" + fractions[currentPosition] + "," + "currentPos: " + currentPosition);
-                Log.i("previousFraction","3:PreFraction:" + fractions[currentPosition + 1] + "," + "previousPos: " + (currentPosition + 1));
-                Log.i("previousState" ,"previousState:" + 3);
-                previousState = 3;
-            } else {
-                Log.i("joinFraction","currentPos:" + positionOffset + ",nextPos:" + (1- positionOffset));
-                if(positionOffset <= 0.25){
-                    positionOffset = 0f;
-                }
-                setJoiningFraction(currentPosition ,positionOffset);
-                setJoiningFraction(currentPosition - 1 , 1 - positionOffset);
-                Log.i("previousState" ,"previousState:" + 2);
-                previousState = 2;
-            }
-
-        } else {
-            //不移动
-            previousState = 0;
         }
-        if(lastoffset){
-            previousOffset = 0;
-        } else {
-            previousOffset = positionOffset;
-        }
+        ViewCompat.postInvalidateOnAnimation(this);
     }
 
     @Override
     public void onPageSelected(int position) {
-//        if (isAttachedToWindow) {
-            // this is the main event we're interested in!
-        Log.i("selectPos","selectPos:" + position);
-            setSelectedPage(position);
-//        } else {
-//            // when not attached, don't animate the move, just store immediately
-//
-//        }
     }
 
     @Override
     public void onPageScrollStateChanged(int state) {
-        if(state == ViewPager.SCROLL_STATE_IDLE) {
-            Log.i("scrollState" , "state_idle");
-        } else if(state == ViewPager.SCROLL_STATE_DRAGGING){
-            pageChanging = false;
-            Log.i("scrollState" , "state_drag");
-        } else if(state == ViewPager.SCROLL_STATE_SETTLING){
-            Log.i("scrollState" , "state_settl");
-        }
     }
 
-    private void setSelectedPage(int now) {
-        if (now == currentPage) return;
-
-        pageChanging = true;
-        previousPage = currentPage;
-        currentPage = now;
-        for(int i = 0 ; i < mPageCount ; i++){
-            if(i != currentPage && i != previousPage){
-                fractions[i] = 0f;
-            }
-        }
-//        final int steps = Math.abs(now - previousPage);
-//
-//        if (steps > 1) {
-//            if (now > previousPage) {
-//                for (int i = 0; i < steps; i++) {
-//                    setJoiningFraction(previousPage + i, 1f);
-//                }
-//            } else {
-//                for (int i = -1; i > -steps; i--) {
-//                    setJoiningFraction(previousPage + i, 1f);
-//                }
-//            }
-//        }
-    }
-
-    private void setJoiningFraction(int dot, float fraction) {
-        if (dot < fractions.length) {
-
-            if (dot == 1) {
-                //Log.d("PageIndicator", "dot 1 fraction:\t" + fraction);
-            }
-
-            fractions[dot] = fraction;
-          handler.sendMessage(obtainMessage(1));
-        }
-    }
-
-    private Message obtainMessage(int what){
-        Message msg = new Message();
-        msg.what = what;
-        return msg;
-    }
-
-    @Override
-    public void onViewAttachedToWindow(View v) {
-        isAttachedToWindow = true;
-    }
-
-    @Override
-    public void onViewDetachedFromWindow(View v) {
-        isAttachedToWindow = false;
-    }
 }
